a66e49d9f4622af6b2045f94bea9783f2eca87f1
Satish Patil <satish.patil7017@gmail.com>
2022-08-21 21:03:50 +0530

    [Satish] Added Starting game with player two and winning report

--- SnakeLadderProblem/SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/SnakeLadderProblem/Program.cs
@@ -7,6 +7,7 @@ namespace SnakeAndLadderProblems
         static void Main(string[] args)
         {
             Console.WriteLine("==========Welcome To Snake And Ladder Game========");
+            Console.WriteLine("Initital Position of both Players is 0");
             SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
             snakeAndLadder.StartGame();
             Console.ReadLine();
--- SnakeLadderProblem/SnakeLadderProblem/SnakeAndLadder.cs
+++ SnakeLadderProblem/SnakeLadderProblem/SnakeAndLadder.cs
@@ -8,60 +8,116 @@ namespace SnakeAndLadderProblems
 {
     public class SnakeAndLadder
     {
-        private int PlayerPosition;
+        public int PlayerPosition1 = 0;
+        public int PlayerPosition2 = 0;
         public const int No_Play = 0;
         public const int Ladder = 1;
         public const int Snake = 2;
         public const int Winning = 100;
-        int StartPoint = 0;
-        int Count = 0;
+        public int StartPoint = 0;
+        public int DieCountPlayer1 = 0;
+        public int DieCountPlayer2 = 0;
+        public int TotalDieCount = 0;
+
+
         public void StartGame()
         {
             Console.WriteLine(" Game Started ");
-            Console.WriteLine(" PlayerPosition :" + PlayerPosition);
+
             Random random = new Random();
 
-            while (StartPoint < Winning)
+            while (PlayerPosition1 != Winning || PlayerPosition2 != Winning)
             {
-                Count++;
+            Player1Chance:
                 int DieRolled = random.Next(1, 7);
-                int Option = random.Next(0, 3);
-                switch (Option)
+                Console.Write(" Dice Rolled for Player1: {0} ", DieRolled);
+
+                int Option1 = random.Next(0, 3);
+                Console.WriteLine(" Move Option : " + Option1);
+
+                TotalDieCount++;
+                DieCountPlayer1++;
+
+                switch (Option1)
                 {
                     case No_Play:
                         Console.WriteLine(" No Play");
                         break;
                     case Ladder:
-                        StartPoint += DieRolled;
-                        Console.WriteLine(" Dice Rolls Number : +{0} ", DieRolled);
-                        Console.WriteLine(" Got Ladder : " + StartPoint);
-                        if (StartPoint > Winning)
+                        PlayerPosition1 += DieRolled;
+
+                        if (PlayerPosition1 > Winning)
                         {
-                            StartPoint -= DieRolled;
+                            PlayerPosition1 -= DieRolled;
+                            Console.WriteLine("Can't move ahead of 100");
+                            break;
+
                         }
-                        Console.WriteLine(" PlayerPosition : " + StartPoint);
-                        break;
+                        Console.WriteLine("Player1 got Ladder, New Position : {0}", PlayerPosition1);
+                        goto Player1Chance;
+
+
                     case Snake:
-                        StartPoint -= DieRolled;
-                        Console.WriteLine(" Dice Rolls Number : -{0} ", DieRolled);
-                        Console.WriteLine(" Snake Attack : " + StartPoint);
-
-                        if (StartPoint < 0)
+                        PlayerPosition1 -= DieRolled;
+                        if (PlayerPosition1 < 0)
                         {
-                            StartPoint = 0;
-
+                            PlayerPosition1 = StartPoint;
                         }
-                        Console.WriteLine(" PlayerPosition : " + StartPoint);
+                        Console.WriteLine("Player1 got Snake, New Position : {0}", PlayerPosition1);
                         break;
                 }
-                if (StartPoint == Winning)
+            Player2Chance:
+                DieRolled = random.Next(1, 7);
+                Console.Write(" Dice Rolled for Player2: {0} ", DieRolled);
+
+                int Option2 = random.Next(0, 3);
+                Console.WriteLine(" Move Option : " + Option2);
+
+                TotalDieCount++;
+                DieCountPlayer2++;
+
+                switch (Option2)
                 {
-                    Console.WriteLine(" NO of times dice rolled : {0} ", +Count);
-                    Console.WriteLine(" Congratulations Player won");
-                    Console.WriteLine("****GAME END****");
-                    break;
+                    case No_Play:
+                        Console.WriteLine(" No Play");
+                        break;
+                    case Ladder:
+                        PlayerPosition2 += DieRolled;
+
+                        if (PlayerPosition2 > Winning)
+                        {
+                            PlayerPosition2 -= DieRolled;
+                            Console.WriteLine("Can't move ahead of 100");
+                            break;
+
+                        }
+                        Console.WriteLine("Player2 got Ladder, New Position : {0}", PlayerPosition2);
+                        goto Player2Chance;
+
+
+                    case Snake:
+                        PlayerPosition2 -= DieRolled;
+                        if (PlayerPosition2 < 0)
+                        {
+                            PlayerPosition2 = StartPoint;
+                        }
+                        Console.WriteLine("Player2 got Snake, New Position : {0}", PlayerPosition2);
+                        break;
                 }
             }
+            if (PlayerPosition1 == Winning)
+            {
+                Console.WriteLine("Congratulations Player1 won the Game");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations Player2 won the Game");
+
+            }
+            Console.WriteLine("Total Number of Die thrown: " + TotalDieCount);
+            Console.WriteLine("Total Number of Die thrown by Player1: " + DieCountPlayer1);
+            Console.WriteLine("Total Number of Die thrown by Player2: " + DieCountPlayer2);
+
         }
     }
 }
